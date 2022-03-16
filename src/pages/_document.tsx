9f99ffabe5fbf8a4b2d70fec6295bd7e280d3e8e
MatheusFontenele <matheus.fontenele@icloud.com>
2022-03-16 12:05:09 -0300

    feat: create a _document file

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { ColorModeScript } from '@chakra-ui/react'
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import theme from '../styles/theme'
+
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          {/* 👇 Here's the script */}
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
