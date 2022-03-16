05b11de67a04aa5765c8221d52e66a577df23678
MatheusFontenele <matheus.fontenele@icloud.com>
2022-03-16 15:45:39 -0300

    feat: complete navbar structure

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,29 +2,27 @@ import { Text, useColorModeValue } from '@chakra-ui/react'
 import styled from '@emotion/styled'
 import Image from 'next/image'
 import Link from 'next/link'
+import logoImage from '../../assets/Logo.png'
+import logoImageWhite from '../../assets/Math-white.png'
 
 const LogoBox = styled.span`
-  font-weight: bold
-  font-size: 18px
-  display: inline-flex
-  aling-items: center
-  height: 30px
-  line-height: 30px
-  padding: 10px
-
-  &:hover img {
-    transform: rotate(20deg) 
-  }
+  font-weight: bold;
+  font-size: 18px;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  height: 60px;
+  line-height: 30px;
+  padding: 10px;
 `
 const Logo = () => {
-  const logoImage = `/src/assets/Math${useColorModeValue('', '-white')}.png`
   return (
     <Link href="/">
       <a>
         <LogoBox>
-          <Image src={logoImage} width={20} height={20} alt="logo" />
+          <Image src={logoImage} width={50} height={50} alt="logo" />
           <Text
-            color={useColorModeValue('gray.800', 'whiteAlpha.900')}
+            color={useColorModeValue('gray.900', 'whiteAlpha.900')}
             fontFamily="M PLUS Rounder 1c"
             fontWeight="bold"
             ml={30}
@@ -36,3 +34,5 @@ const Logo = () => {
     </Link>
   )
 }
+
+export default Logo
