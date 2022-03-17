ddf1e7ceb01dba0e783b72424ee790e7dada4a07
MatheusFontenele <matheus.fontenele@icloud.com>
2022-03-17 00:10:51 -0300

    feat: add section and create a social midia section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,27 @@ import {
   Container,
   Heading,
   Image,
-  useColorModeValue
+  useColorModeValue,
+  Text,
+  List,
+  ListItem,
+  Link,
+  IconButton,
+  Button,
+  Icon
 } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import profilePhoto from '../../assets/profile-pic.png'
+import Paragraph from '../components/paragraph'
+import Section from '../components/sections'
+import {
+  IoLogoTwitter,
+  IoLogoInstagram,
+  IoLogoGithub,
+  IoLogoDiscord,
+  IoLogoLinkedin
+} from 'react-icons/io5'
+import { NodeNextRequest } from 'next/dist/server/base-http/node'
 
 const Home: NextPage = () => {
   return (
@@ -21,32 +38,89 @@ const Home: NextPage = () => {
       >
         Hello, I&apos;m a full-stack developer based in Brazil!
       </Box>
-      <Box display={{ md: 'flex' }}>
+
+      <Box flexShrink={0} mt={{ base: 4, md: 0 }} mb={6}>
+        <Image
+          src="https://avatars.githubusercontent.com/u/94790993?v=4"
+          maxW="150px"
+          display="inline-block"
+          alt="profile Photo"
+          borderRadius="full"
+        />
+      </Box>
+
+      <Box display={{ md: 'flex' }} mb={6}>
         <Box flexGrow={1}>
           <Heading as="h2" variant="page-title">
-            Matheus Fontenele
+            Hi, I'm <br /> Matheus Fontenele
           </Heading>
-          <p>Web Developer, UI designer and lover of JavaScript </p>
-        </Box>
-        <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
-          display="flex"
-          justifyContent="center"
-        >
-          <Image
-            src="https://avatars.githubusercontent.com/u/94790993?v=4"
-            maxW="100px"
-            display="inline-block"
-            alt="profile Photo"
-            borderColor="whiteAlpha.800"
-            borderStyle="solid"
-            borderWidth={2}
-            borderRadius="full"
-          />
+          <Text>Web Developer, UI designer and lover of JavaScript </Text>
         </Box>
       </Box>
+
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Me
+        </Heading>
+        <Paragraph>
+          Matheus is someone who is passionate about technology, sports and
+          history. Someone who is fascinated to learn new things involving code
+          lines, always looking improve my knowledge to solve the more simple
+          real-life problem with code{' '}
+        </Paragraph>
+      </Section>
+
+      <Section delay={0.3}>
+        <List display="flex" flexDirection={{ base: 'column', md: 'row' }}>
+          <ListItem>
+            <Link href="/">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<Icon as={IoLogoGithub} />}
+              >
+                Github
+              </Button>
+            </Link>
+          </ListItem>
+
+          <ListItem>
+            <Link href="/">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<Icon as={IoLogoInstagram} />}
+              >
+                Instagram
+              </Button>
+            </Link>
+          </ListItem>
+
+          <ListItem>
+            <Link href="/">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<Icon as={IoLogoTwitter} />}
+              >
+                Twitter
+              </Button>
+            </Link>
+          </ListItem>
+
+          <ListItem>
+            <Link href="/">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<Icon as={IoLogoLinkedin} />}
+              >
+                Linkedin
+              </Button>
+            </Link>
+          </ListItem>
+        </List>
+      </Section>
     </Container>
   )
 }
