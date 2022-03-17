882a97a694922c215c60694ba0a1772efaa1bacd
MatheusFontenele <matheus.fontenele@icloud.com>
2022-03-17 00:08:11 -0300

    refactor: change color button

--- src/components/theme-toggle-button.tsx
+++ src/components/theme-toggle-button.tsx
@@ -1,12 +1,13 @@
 import { SunIcon, MoonIcon } from '@chakra-ui/icons'
 import { IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react'
+import { AnimatePresence, motion } from 'framer-motion'
 const ThemeToggleButton = () => {
   const { toggleColorMode } = useColorMode()
 
   return (
     <IconButton
       aria-label="Toggle Button"
-      colorScheme={useColorModeValue('purple', 'orange')}
+      colorScheme={useColorModeValue('purple', 'yellow')}
       icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
       onClick={toggleColorMode}
     ></IconButton>
