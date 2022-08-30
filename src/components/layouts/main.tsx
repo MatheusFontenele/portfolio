4c27518d7dcb4c91a51acf2799e4819dae85d83d
MatheusFontenele <matheus.fontenele@icloud.com>
2022-08-29 23:30:09 -0300

    feat: init sidebar component

--- src/components/layouts/main.tsx
+++ src/components/layouts/main.tsx
@@ -2,6 +2,7 @@ import { Box, Container } from '@chakra-ui/react'
 import Head from 'next/head'
 import React from 'react'
 import NavBar from '../navbar'
+import { Sidebar } from '../sidebar'
 
 const Main = ({ children, router }: any) => {
   return (
@@ -10,6 +11,7 @@ const Main = ({ children, router }: any) => {
         <title>Home | Matheus Fontenele</title>
       </Head>
       <NavBar path={router.asPath} />
+      <Sidebar />
       <div>{children}</div>
     </Box>
   )
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,7 @@
+export function Sidebar(){
+  return(
+    <div className="fixed left-0 top-0 z-30 h-screen w-[500px] bg-blue-800 backdrop-blur-sm backdrop-brightness-150">
+      <h1>hello world</h1>
+    </div>
+  )
+}
