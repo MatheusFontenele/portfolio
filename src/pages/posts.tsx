e4e458e21cc94aa4df978253edaf9d36fc54d9d2
MatheusFontenele <matheus.fontenele@icloud.com>
2023-03-04 22:34:12 -0300

    feat: add headles ui

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,5 +1,39 @@
+import { Disclosure } from '@headlessui/react'
+import { BsChevronDown } from 'react-icons/bs';
+
 function Posts() {
   return (
+    <div className="w-full px-4 pt-16">
+      <div className="mx-auto w-full max-w-md rounded bg-white p-2">
+        <Disclosure>
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex w-full justify-between rounded bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                <span>What is your refund policy?</span>
+                <BsChevronDown />
+              </Disclosure.Button>
+              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                If you're unhappy with your purchase for any reason, email us
+                within 90 days and we'll refund you in full, no questions asked.
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <Disclosure as="div" className="mt-2">
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex w-full justify-between rounded bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                <span>Do you offer technical support?</span>
+                <BsChevronDown />
+              </Disclosure.Button>
+              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                No.
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+      </div>
+    </div>
   );
 }
 
