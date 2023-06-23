63a64ed007349d8905b23b95bcc5fe8a4bc44f4c
MatheusFontenele <matheus.fontenele@icloud.com>
2023-06-23 16:16:54 +0000

    refactor: adicionado apple login

--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -1,7 +1,9 @@
+import AppleLogin from 'react-apple-login'
+
 function Works() {
   return (
     <div className="w-screen h-screen">
-      <h1>Works</h1>
+      <AppleLogin clientId="com.imirante.login" redirectURI="" />
     </div>
   )
 }
